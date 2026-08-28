5bfea509a2ed3c31dcda3cf3ec28abf0e5b57cd1
Parv Chaudhary <parvchaudhary040@gmail.com>
2026-08-28 09:59:41 +0530

    Time: 51 ms (25.26%), Space: 19.2 MB (64.55%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        rev = 0
+        for i in range(32):
+            bit = n&1
+            rev = (rev << 1) | bit
+            n >>= 1
+        return rev
